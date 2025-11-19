58031f34d94f01e9e77374d4098caf6b5f6ed109
Mathias Moen <mathiasbbmoen@gmail.com>
2025-11-19 11:09:14 +0100

    Postit is now draggable

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ console.log(goal);
 let goal_text = document.getElementById("goal");
 goal_text.innerText = goal;
 
-let postit = document.getElementsByClassName("postit");
+let postit = document.querySelector(".postit");
 
-function click() {
-  console.log("click");
-}
+let isDragging = false;
+let offsetX, offsetY;
+
+postit.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  // Calculate offset between mouse and postit's top-left corner
+  offsetX = e.clientX - postit.offsetLeft;
+  offsetY = e.clientY - postit.offsetTop;
+  postit.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    // Update position to follow cursor, accounting for offset
+    postit.style.left = e.clientX - offsetX + "px";
+    postit.style.top = e.clientY - offsetY + "px";
+  }
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  postit.style.cursor = "grab";
+});
